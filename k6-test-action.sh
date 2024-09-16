311af0d9a1627e0bc4b84a4f26ac384cc35dd088
noneill <noneill@patriotsoftware.com>
2024-09-16 14:31:43 -0400

    Testing

--- k6-test-action.sh
+++ k6-test-action.sh
@@ -6,6 +6,7 @@ if [[ -n "${INPUT_BASE_URL_OVERRIDE}" ]]; then
     echo "initial-base-url=$(grep "BASE_URL" ${INPUT_K6_SCRIPT} | head -1)" >> $GITHUB_OUTPUT
     sed -i "s,const BASE_URL =.*,const BASE_URL = \"${INPUT_BASE_URL_OVERRIDE}\";,g" $INPUT_K6_SCRIPT
     echo "resulting-base-url=$(grep "BASE_URL" ${INPUT_K6_SCRIPT} | head -1)" >> $GITHUB_OUTPUT
+    echo "${INPUT_BASE_URL_OVERRIDE}"
 fi
 
 echo "Starting K6 after 60s warm-up delay..."
